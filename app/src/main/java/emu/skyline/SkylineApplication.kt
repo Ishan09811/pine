20a6dae6a51477bbb4953a81f6609d8fd38dc364
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-12-20 18:36:19 +0530

    Update SkylineApplication.kt

--- app/src/main/java/emu/skyline/SkylineApplication.kt
+++ app/src/main/java/emu/skyline/SkylineApplication.kt
@@ -7,11 +7,14 @@ package emu.skyline
 
 import android.app.Application
 import android.content.Context
+import android.graphics.Color
+import androidx.annotation.ColorInt
 import com.google.android.material.color.DynamicColors
 import com.google.android.material.color.DynamicColorsOptions
 import dagger.hilt.android.HiltAndroidApp
 import emu.skyline.di.getSettings
 import java.io.File
+import kotlin.math.roundToInt
 
 /**
  * @return The optimal directory for putting public files inside, this may return a private directory if a public directory cannot be retrieved
@@ -29,6 +32,24 @@ class SkylineApplication : Application() {
             private set
 
         val context : Context get() = instance.applicationContext
+
+        /**
+         * Adjusts the opacity of a color by applying an alpha factor.
+         *
+         * @param color The original color (including alpha).
+         * @param alphaFactor A value between 0.0 (fully transparent) and 1.0 (no change in opacity).
+         * @return A new color with the adjusted opacity.
+        */
+        @ColorInt
+        fun applyAlphaToColor(@ColorInt color: Int, alphaFactor: Float): Int {
+            val newAlpha = (Color.alpha(color) * alphaFactor).coerceIn(0f, 255f).roundToInt()
+            return Color.argb(
+                newAlpha,
+                Color.red(color),
+                Color.green(color),
+                Color.blue(color)
+            )
+        }
     }
 
     override fun onCreate() {
