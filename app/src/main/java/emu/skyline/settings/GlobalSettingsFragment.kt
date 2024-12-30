15c61f8e27f530f1e15a2f5d8baad064e11d9bfe
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2024-12-30 15:13:08 +0530

    missing import

--- app/src/main/java/emu/skyline/settings/GlobalSettingsFragment.kt
+++ app/src/main/java/emu/skyline/settings/GlobalSettingsFragment.kt
@@ -7,6 +7,7 @@ package emu.skyline.settings
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Build
 import android.view.View
 import androidx.preference.Preference
 import androidx.preference.PreferenceCategory
