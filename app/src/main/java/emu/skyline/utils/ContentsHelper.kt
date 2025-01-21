689805c4eaaf59589afcd2604e61118ca2d57d64
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-01-21 19:26:28 +0530

    Create ContentsHelper.kt

---
+++ app/src/main/java/emu/skyline/utils/ContentsHelper.kt
@@ -0,0 +1,41 @@
+
+package emu.skyline
+
+import android.content.Context
+import java.io.*
+
+class ContentsHelper(private val context: Context) {
+
+    private val fileName = "contents.dat"
+
+    fun saveContents(contents: List<Serializable>) {
+        try {
+            val fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE)
+            val objectOutputStream = ObjectOutputStream(fileOutputStream)
+            objectOutputStream.writeObject(contents)
+            objectOutputStream.close()
+            fileOutputStream.close()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun loadContents(): List<Serializable> {
+        return try {
+            val fileInputStream = context.openFileInput(fileName)
+            val objectInputStream = ObjectInputStream(fileInputStream)
+            val contents = objectInputStream.readObject() as List<Serializable>
+            objectInputStream.close()
+            fileInputStream.close()
+            contents
+        } catch (e: FileNotFoundException) {
+            emptyList()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            emptyList()
+        } catch (e: ClassNotFoundException) {
+            e.printStackTrace()
+            emptyList()
+        }
+    }
+}
