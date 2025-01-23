a45dccdfa38181916a5dd9de6648e2bac95325d1
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-01-23 17:24:19 +0530

    Update ContentsHelper.kt

--- app/src/main/java/emu/skyline/utils/ContentsHelper.kt
+++ app/src/main/java/emu/skyline/utils/ContentsHelper.kt
@@ -79,7 +79,7 @@ class ContentsHelper(private val context: Context) {
         return null
     }
 
-    private fun getUriSize(context: Context, uri: Uri): Long? {
+    fun getUriSize(context: Context, uri: Uri): Long? {
         var size: Long? = null
         context.contentResolver.query(uri, null, null, null, null)?.use { cursor ->
             val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
