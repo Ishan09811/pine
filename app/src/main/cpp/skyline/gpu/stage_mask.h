06492e32b79d62294e9800f71a1300129418e525
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-11-04 14:54:28 +0530

    Create stage_mask.h

---
+++ app/src/main/cpp/skyline/gpu/stage_mask.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+
+namespace skyline::gpu {
+
+/**
+ * @brief A wrapper for vk::PipelineStageFlags / vk::PipelineStageFlags2
+ *        that allows code to work with both legacy and Synchronization2.
+ */
+struct StageMask {
+    uint64_t mask{0};
+
+    constexpr StageMask() = default;
+
+    constexpr StageMask(vk::PipelineStageFlags flags)
+        : mask(static_cast<uint64_t>(flags)) {}
+    constexpr StageMask(vk::PipelineStageFlags2 flags)
+        : mask(static_cast<uint64_t>(flags)) {}
+
+    constexpr operator vk::PipelineStageFlags() const {
+        return static_cast<vk::PipelineStageFlags>(mask);
+    }
+    constexpr operator vk::PipelineStageFlags2() const {
+        return static_cast<vk::PipelineStageFlags2>(mask);
+    }
+
+    constexpr StageMask& operator|=(StageMask rhs) {
+        mask |= rhs.mask;
+        return *this;
+    }
+    constexpr StageMask operator|(StageMask rhs) const {
+        return StageMask{mask | rhs.mask};
+    }
+
+    constexpr bool operator==(const StageMask& rhs) const {
+        return mask == rhs.mask;
+    }
+    constexpr bool operator!=(const StageMask& rhs) const {
+        return mask != rhs.mask;
+    }
+
+    constexpr StageMask operator&(StageMask rhs) const {
+        return StageMask{mask & rhs.mask};
+    }
+};
+
+} // namespace skyline::gpu
