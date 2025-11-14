d0afe2cdecbade279e23dfde420495c9ad55b6fa
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-11-14 21:38:52 +0530

    Update compressed_backing.h

--- app/src/main/cpp/skyline/vfs/compressed_backing.h
+++ app/src/main/cpp/skyline/vfs/compressed_backing.h
@@ -3,30 +3,32 @@
 
 #include "backing.h"
 
-class CompressedBacking : public Backing {
-public:
-    CompressedBacking(std::shared_ptr<Backing> rawCompressedData);
+namespace skyline::vfs {
+    class CompressedBacking : public Backing {
+    public:
+        CompressedBacking(std::shared_ptr<Backing> rawCompressedData);
 
-    size_t ReadImpl(span<u8> output, size_t offset) override;
-    size_t WriteImpl(span<u8> input, size_t offset) override { 
-        throw exception("CompressedBacking is read-only");
-    }
-    void ResizeImpl(size_t) override { 
-        throw exception("CompressedBacking does not support resize");
-    }
+        size_t ReadImpl(span<u8> output, size_t offset) override;
+        size_t WriteImpl(span<u8> input, size_t offset) override { 
+            throw exception("CompressedBacking is read-only");
+        }
+        void ResizeImpl(size_t) override { 
+            throw exception("CompressedBacking does not support resize");
+        }
 
-private:
-    struct Header {
-        u32 magic;
-        u32 blockSize;
-        u64 uncompressedSize;
-        u64 compressedSize;
-        u32 blockCount;
-        std::vector<u32> blockOffsets;
+    private:
+        struct Header {
+            u32 magic;
+            u32 blockSize;
+            u64 uncompressedSize;
+            u64 compressedSize;
+            u32 blockCount;
+            std::vector<u32> blockOffsets;
+        };
+
+        Header hdr;
+        std::shared_ptr<Backing> compressedBacking;
+
+        void ReadBlock(size_t blockIndex, span<u8> out);
     };
-
-    Header hdr;
-    std::shared_ptr<Backing> compressedBacking;
-
-    void ReadBlock(size_t blockIndex, span<u8> out);
-};
+}
