927e97a746803aefac46474ca76c4208ee5761b8
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-11-14 19:10:43 +0530

    Create compressed_backing.h

---
+++ app/src/main/cpp/skyline/vfs/compressed_backing.h
@@ -0,0 +1,27 @@
+class CompressedBacking : public Backing {
+public:
+    CompressedBacking(std::shared_ptr<Backing> rawCompressedData);
+
+    size_t ReadImpl(span<u8> output, size_t offset) override;
+    size_t WriteImpl(span<u8> input, size_t offset) override { 
+        throw exception("CompressedBacking is read-only");
+    }
+    void ResizeImpl(size_t) override { 
+        throw exception("CompressedBacking does not support resize");
+    }
+
+private:
+    struct Header {
+        u32 magic;
+        u32 blockSize;
+        u64 uncompressedSize;
+        u64 compressedSize;
+        u32 blockCount;
+        std::vector<u32> blockOffsets;
+    };
+
+    Header hdr;
+    std::shared_ptr<Backing> compressedBacking;
+
+    void ReadBlock(size_t blockIndex, span<u8> out);
+};
