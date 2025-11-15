972ee4929d92af73469aae8c86632f41bdc519a6
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-11-15 17:19:01 +0530

    Update nca.cpp

--- app/src/main/cpp/skyline/vfs/nca.cpp
+++ app/src/main/cpp/skyline/vfs/nca.cpp
@@ -145,7 +145,7 @@ namespace skyline::vfs {
             subsectionBuckets.back().entries.push_back({size, {0}, 0});
 
             auto key{!(rightsIdEmpty || useKeyArea) ? GetTitleKey() : GetKeyAreaKey(sectionHeader.raw.header.encryptionType)};
-            auto bktrBacking{sectionHeader.raw.compressionInfo.bucket.tableOffset != 0 ? std::make_shared<CompressedBacking>(std::make_shared<RegionBacking>(backing, baseOffset, romFsSize)) : std::make_shared<RegionBacking>(backing, baseOffset, romFsSize)};
+            std::shared_ptr<Backing> bktrBacking = sectionHeader.raw.compressionInfo.bucket.tableOffset != 0 ? std::make_shared<CompressedBacking>(std::make_shared<RegionBacking>(backing, baseOffset, romFsSize)) : std::make_shared<RegionBacking>(backing, baseOffset, romFsSize);
 
             auto bktr{std::make_shared<BKTR>(
                 bktrBaseRomfs, bktrBacking,
