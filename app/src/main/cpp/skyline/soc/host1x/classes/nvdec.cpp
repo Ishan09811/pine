b045f7063a573615596fed092ece8a756e4308ae
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-11-19 02:24:23 +0530

    nvdec: fill registers

--- app/src/main/cpp/skyline/soc/host1x/classes/nvdec.cpp
+++ app/src/main/cpp/skyline/soc/host1x/classes/nvdec.cpp
@@ -4,10 +4,23 @@
 #include "nvdec.h"
 
 namespace skyline::soc::host1x {
+    #define NVDEC_INDEX(fieldName)                                                                \
+        (offsetof(NvdecRegisters, fieldName) / sizeof(u64))
+
     NvDecClass::NvDecClass(std::function<void()> opDoneCallback)
         : opDoneCallback(std::move(opDoneCallback)) {}
 
     void NvDecClass::CallMethod(u32 method, u32 argument) {
         LOGW("Unknown NVDEC class method called: 0x{:X} argument: 0x{:X}", method, argument);
+        registers.raw[method] = static_cast<u64>(argument) << 8;
+
+        switch (method) {
+            case NVDEC_INDEX(set_codec_id):
+                //TODO: Implement decoder
+                break;
+            case NVDEC_INDEX(execute):
+                //TODO: Execute
+                break;
+        }
     }
 }
