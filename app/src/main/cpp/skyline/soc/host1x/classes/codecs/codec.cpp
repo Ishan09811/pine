5f53f0d0d89af1bf68419d66fcec76bd224cdf33
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-11-23 13:31:08 +0530

    Create  codec.cpp

---
+++ app/src/main/cpp/skyline/soc/host1x/classes/codecs/codec.cpp
@@ -0,0 +1,84 @@
+
+#include "soc/host1x/codecs/codec.h"
+#include "soc/host1x/codecs/h264.h"
+
+namespace skyline::soc::host1x {
+
+Codec::Codec(const DeviceState &state, const NvdecRegisters& regs)
+    : deviceState(state), state{regs}, h264Decoder(std::make_unique<Decoder::H264>(deviceState)) {}
+
+Codec::~Codec() = default;
+
+void Codec::Initialize() {
+    initialized = decodeAPI.Initialize(currentCodec);
+}
+
+void Codec::SetTargetCodec(VideoCodec codec) {
+    if (currentCodec != codec) {
+        currentCodec = codec;
+        LOGI("NVDEC video codec initialized to {}", GetCurrentCodecName());
+    }
+}
+
+void Codec::Decode() {
+    const bool isFirstFrame = !initialized;
+    if (isFirstFrame) {
+        Initialize();
+    }
+
+    if (!initialized) {
+        return;
+    }
+
+    // Assemble bitstream.
+    size_t configurationSize = 0;
+    const auto packetData = [&]() {
+        switch (current_codec) {
+        case VideoCodec::H264:
+            return h264Decoder->ComposeFrame(state, &configurationSize, isFirstFrame);
+        default:
+            return std::span<const u8>{};
+        }
+    }();
+
+    // Send assembled bitstream to decoder.
+    if (!decodeAPI.SendPacket(packetData, configurationSize)) {
+        return;
+    }
+
+    // Receive output frames from decoder.
+    decodeAPI.ReceiveFrames(frames);
+
+    while (frames.size() > 10) {
+        LOGD("ReceiveFrames overflow, dropped frame");
+        frames.pop();
+    }
+}
+
+std::unique_ptr<FFmpeg::Frame> Codec::GetCurrentFrame() {
+    // Sometimes VIC will request more frames than have been decoded.
+    // in this case, return a blank frame and don't overwrite previous data.
+    if (frames.empty()) {
+        return {};
+    }
+
+    auto frame = std::move(frames.front());
+    frames.pop();
+    return frame;
+}
+
+VideoCodec Codec::GetCurrentCodec() const {
+    return currentCodec;
+}
+
+std::string_view Codec::GetCurrentCodecName() const {
+    switch (currentCodec) {
+    case VideoCodec::None:
+        return "None";
+    case VideoCodec::H264:
+        return "H264";
+    default:
+        return "Unknown";
+    }
+}
+} // namespace skyline
