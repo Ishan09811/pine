0ce669f48de7661374f5697c195a3b9c75cb9f22
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-11-23 17:26:03 +0530

    Update nvdec.cpp

--- app/src/main/cpp/skyline/soc/host1x/classes/nvdec.cpp
+++ app/src/main/cpp/skyline/soc/host1x/classes/nvdec.cpp
@@ -7,23 +7,34 @@ namespace skyline::soc::host1x {
     #define NVDEC_INDEX(fieldName) (decltype(registers.fieldName)::kOffset / sizeof(u64))
 
     NvDecClass::NvDecClass(std::function<void()> opDoneCallback, const DeviceState &state)
-        : opDoneCallback(std::move(opDoneCallback)), state(state) {}
+        : opDoneCallback(std::move(opDoneCallback)), codec(std::make_unique<Codec>(state, registers)) {}
 
     void NvDecClass::CallMethod(u32 method, u32 argument) {
-        LOGW("Unknown NVDEC class method called: 0x{:X} argument: 0x{:X}", method, argument);
+        LOGW("NVDEC class method called: 0x{:X} argument: 0x{:X}", method, argument);
         registers.raw[method] = static_cast<u64>(argument) << 8;
 
         switch (method) {
             case NVDEC_INDEX(setCodecId):
-                //TODO: Implement decoder
+                codec->SetTargetCodec(static_cast<VideoCodec>(argument));
                 break;
             case NVDEC_INDEX(execute):
-                //TODO: Execute
+                Execute();
                 break;
         }
     }
 
+    std::unique_ptr<FFmpeg::Frame> NvDecClass::GetFrame() {
+        return codec->GetCurrentFrame();
+    }
+
     void NvDecClass::Execute() {
-        // TODO: Implement decoder
+        switch (codec->GetCurrentCodec()) {
+            case NvdecCommon::VideoCodec::H264:
+                codec->Decode();
+                break;
+            default:
+                LOGW("Codec {}", codec->GetCurrentCodecName());
+                break;
+        }
     }
 }
