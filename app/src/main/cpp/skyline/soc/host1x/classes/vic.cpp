ff1186a6c0346f86df0c9a3085f5d1ef90b29e4a
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2025-11-24 13:01:24 +0530

    Update vic.cpp

--- app/src/main/cpp/skyline/soc/host1x/classes/vic.cpp
+++ app/src/main/cpp/skyline/soc/host1x/classes/vic.cpp
@@ -21,58 +21,58 @@ namespace skyline::soc::host1x {
 
     union VicConfig {
         u64_le raw{};
-        BitField<0, 7, VideoPixelFormat> pixel_format;
-        BitField<7, 2, u64_le> chroma_loc_horiz;
-        BitField<9, 2, u64_le> chroma_loc_vert;
-        BitField<11, 4, u64_le> block_linear_kind;
-        BitField<15, 4, u64_le> block_linear_height_log2;
-        BitField<32, 14, u64_le> surface_width_minus1;
-        BitField<46, 14, u64_le> surface_height_minus1;
+        BitField<0, 7, VideoPixelFormat> pixelFormat;
+        BitField<7, 2, u64_le> chromaLocHoriz;
+        BitField<9, 2, u64_le> chromaLocVert;
+        BitField<11, 4, u64_le> blockLinearKind;
+        BitField<15, 4, u64_le> blockLinearHeightLog2;
+        BitField<32, 14, u64_le> surfaceWidthMinus1;
+        BitField<46, 14, u64_le> surfaceHeightMinus1;
     };
 
     VicClass::VicClass(std::function<void()> opDoneCallback, const DeviceState &state)
         : opDoneCallback(std::move(opDoneCallback)), state(state) {}
 
-    void VicClass::CallMethod(u32 method, u32 argument) {
-        LOGW("Unknown VIC class method called: 0x{:X} argument: 0x{:X}", method, argument);
+    void VicClass::CallMethod(Method method, u32 argument) {
+        LOGW("VIC class method called: 0x{:X} argument: 0x{:X}", method, argument);
         const u64 arg = static_cast<u64>(argument) << 8;
-        /*switch (method) {
+        switch (method) {
             case Method::Execute:
                 Execute();
                 break;
             case Method::SetConfigStructOffset:
-                config_struct_address = arg;
+                configStructAddress = arg;
                 break;
             case Method::SetOutputSurfaceLumaOffset:
-                output_surface_luma_address = arg;
+                outputSurfaceLumaAddress = arg;
                 break;
             case Method::SetOutputSurfaceChromaOffset:
-                output_surface_chroma_address = arg;
+                outputSurfaceChromaAddress = arg;
                 break;
             default:
                 break;
-        }*/
+        }
     }
 
     void Vic::Execute() {
-        if (output_surface_luma_address == 0) {
+        if (outputSurfaceLumaAddress == 0) {
             LOGE("VIC Luma address not set.");
             return;
         }
-        const VicConfig config{state.soc->smmu.Read<u64>(config_struct_address + 0x20)};
+        const VicConfig config{state.soc->smmu.Read<u64>(configStructAddress + 0x20)};
         /*auto frame = nvdec_processor->GetFrame();
         if (!frame) {
             return;
         }
-        const u64 surface_width = config.surface_width_minus1 + 1;
-        const u64 surface_height = config.surface_height_minus1 + 1;
-        if (static_cast<u64>(frame->GetWidth()) != surface_width ||
-            static_cast<u64>(frame->GetHeight()) != surface_height) {
+        const u64 surfaceWidth = config.surfaceWidthMinus1 + 1;
+        const u64 surfaceHeight = config.surfaceHeightMinus1 + 1;
+        if (static_cast<u64>(frame->GetWidth()) != surfaceWidth ||
+            static_cast<u64>(frame->GetHeight()) != surfaceHeight) {
             // TODO: Properly support multiple video streams with differing frame dimensions
             LOGW("Frame dimensions {}x{} don't match surface dimensions {}x{}",
-                        frame->GetWidth(), frame->GetHeight(), surface_width, surface_height);
+                        frame->GetWidth(), frame->GetHeight(), surfaceWidth, surfaceHeight);
         }
-        switch (config.pixel_format) {
+        switch (config.pixelFormat) {
             case VideoPixelFormat::RGBA8:
             case VideoPixelFormat::BGRA8:
             case VideoPixelFormat::RGBX8:
@@ -82,7 +82,7 @@ namespace skyline::soc::host1x {
                 WriteYUVFrame(std::move(frame), config);
                 break;
         default:
-            LOGW("Unknown video pixel format {:X}", config.pixel_format.Value());
+            LOGW("Unknown video pixel format {:X}", config.pixelFormat.Value());
             break;
         }*/
     }
